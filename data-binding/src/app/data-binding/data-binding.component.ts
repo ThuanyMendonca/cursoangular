add83d6e07ebab28a6620b14fc46e77a8dec79be
Thuany Mendonca <thuany1997@hotmail.com>
2018-01-29 00:22:37 -0200

    Data-Binding

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -8,7 +8,22 @@ import { Component, OnInit } from '@angular/core';
 export class DataBindingComponent implements OnInit {
   // A declaração da variável como string é opcional
   url: string = 'http://loiane.com';
+  urlImagem = 'http://lorempixel.com/g/400/200/nature/';
+  cursoAngular = true;
 
+  getValor(){
+    return 1;
+  }
+
+  getCurtirCurso(){
+    if(this.cursoAngular == true){
+      return 'Curti o curso';
+    }
+    else {
+      return 'Não curti'
+    }
+    
+  }
   constructor() { }
 
   ngOnInit() {
